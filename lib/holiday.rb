7d9d300596afacc520885e2c88b039f813a84103
Amy Cen <amy.cen@blackrock.com>
2019-04-16 07:12:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.split.join.capitalize!
     holiday.each do |hol, item|
-      puts hol.to_s.capitalize! + items.join(", ").capitalize!
+      puts hol.to_s.capitalize! + item.join(", ").capitalize!
       binding.pry
     end
   end
