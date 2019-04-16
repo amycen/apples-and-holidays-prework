3b89c00b1449087a92d511399553c283564f7852
Amy Cen <amy.cen@blackrock.com>
2019-04-16 07:35:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,19 +80,3 @@ def all_holidays_with_bbq(holiday_hash)
   hol_w_bbq
 end
 
-holiday_supplies = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}
-all_supplies_in_holidays(holiday_supplies)
