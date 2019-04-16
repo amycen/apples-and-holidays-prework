7db2d65371a8fc4202669ff2b08d0393e2020270
Amy Cen <amy.cen@blackrock.com>
2019-04-16 07:29:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  hol_w_bbq = []
+  holiday_hash.each do |season, holiday|
+    hol_w_bbq << holiday if holiday.has_value?("BBQ")
 end
 
 holiday_supplies = {
